a29482d298d9e9e42bb7243955275ab83659c6eb
Xinwei He <1154646635@qq.com>
2016-12-06 16:04:10 +0800

    fix bugs

--- MyParallelCriterion.lua
+++ MyParallelCriterion.lua
@@ -12,8 +12,9 @@ end
 
 function RepeatedCriterion:updateOutput(input, target)
    self.output = 0
+   if self.weights == nil  then self.weights = torch.ones(#input):div(#input) end 
    for i = 1,#input do
-      self.output = self.output + self.weights[i]*criterion:updateOutput(input[i],target)
+      self.output = self.output + self.weights[i]*self.criterion:updateOutput(input[i],target)
    end
    return self.output
 end
@@ -22,7 +23,7 @@ function RepeatedCriterion:updateGradInput(input, target)
    self.gradInput = nn.utils.recursiveResizeAs(self.gradInput, input)
    nn.utils.recursiveFill(self.gradInput, 0)
    for i = 1,#input do
-      nn.utils.recursiveAdd(self.gradInput[i], self.weights[i], criterion:updateGradInput(input[i], target))
+      nn.utils.recursiveAdd(self.gradInput[i], self.weights[i], self.criterion:updateGradInput(input[i], target))
    end
    return self.gradInput
 end
@@ -31,4 +32,3 @@ function RepeatedCriterion:type(type, tensorCache)
    self.gradInput = {}
    return parent.type(self, type, tensorCache)
 end
-
